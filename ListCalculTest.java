a59fb23545df0cf42363fba36cd7116c3ae44f49
sixleafclover <meriel777@outlook.com>
2023-09-06 17:16:59 +0900

    리스트테스트_버전1_2023_9_6

---
+++ ListCalculTest.java
@@ -0,0 +1,30 @@
+package intermediate;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListCalculTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tot=0;
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		ListCalcul calcul = new ListCalcul();
+		
+		System.out.println("국어점수");
+		al.add(sc.nextInt());
+		System.out.println("수학점수");
+		al.add(sc.nextInt());
+		System.out.println("영어점수");
+		al.add(sc.nextInt());
+		
+		
+		for(int num : al) {
+			tot += num;
+		}
+		System.out.printf("총점 : %d",tot);
+		System.out.printf("평균 : %d",tot/al.size());
+
+	}
+
+}
